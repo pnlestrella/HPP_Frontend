8d0d1d602ad2c61236b2927507bcd6eaaf4e8ff5
pnlestrella <pnlestrella@gmail.com>
2025-12-31 06:23:11 +0800

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import path from "path"
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
-
+import { defineConfig } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,4 +11,14 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  server: {
+    host: true, // allows network access
+    port: 5173,
+    strictPort: true,
+    cors: true, 
+    allowedHosts: [
+      "972f56f8bae4.ngrok-free.app",
+      "localhost",
+    ],
+  },
+});
